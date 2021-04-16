596c1f68eaf0f5586769975f267f522a0a46020f
clever-cr <cleverumuhuza@gmail.com>
2021-04-16 00:01:31 -0700

    money watch will help people to budget and save money

---
+++ server.js
@@ -0,0 +1,40 @@
+
+import express from "express";
+import mongoose from "mongoose";
+import bodyParse from "body-parser";
+import dotenv from "dotenv";
+
+
+dotenv.config({ path: "./.env" });
+const app = express();
+app.use(bodyParse.json());
+
+
+
+
+app.use('/', (req, res) => {
+    res.status(200).send({
+        statu: 200,
+        message: "this route doesn't exist"
+    })
+})
+
+const databaseUrl = process.env.DATABASE
+mongoose.connect(databaseUrl, { 
+    useNewUrlParser: true, 
+    useCreateIndex: true, 
+    useUnifiedTopology: true, 
+    useFindAndModify: false }).then(() => { 
+        console.log("db successfully connected") 
+    })
+const port = process.env.PORT;
+
+
+
+
+app.listen(port, () => {
+
+    console.log(`sever is running on port ${port}`);
+})
+
+export default app;
